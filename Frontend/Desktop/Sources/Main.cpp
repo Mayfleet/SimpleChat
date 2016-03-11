6574369695da9c69f281c33ab7ff6a07b2e57303
Andrew Markin <andrew.n.markin@gmail.com>
2016-03-12 00:33:22 +0300

    Checker implemented to prevent other application instances to start

--- Frontend/Desktop/Sources/Main.cpp
+++ Frontend/Desktop/Sources/Main.cpp
@@ -1,4 +1,6 @@
 #include <QApplication>
+#include <QLocalServer>
+#include <QLocalSocket>
 
 #include "MainWindow.h"
 
@@ -9,6 +11,29 @@ int main(int argc, char* argv[])
     application.setApplicationName(PROJECT_NAME);
     application.setOrganizationName(ORGANIZATION_NAME);
     application.setOrganizationDomain(ORGANIZATION_DOMAIN);
+
+    // Check for other instances already running
+
+    QLocalSocket singleInstanceSocket;
+    singleInstanceSocket.connectToServer(PROJECT_NAME);
+
+    if (singleInstanceSocket.waitForConnected(500))
+    {
+        singleInstanceSocket.close();
+        qCritical() << "Application is already started";
+        return EXIT_FAILURE;
+    }
+
+    // Initialize single instance server to prevent other instances to start
+
+    QLocalServer::removeServer(PROJECT_NAME);
+    QLocalServer singleInstanceServer;
+
+    if (!singleInstanceServer.listen(PROJECT_NAME))
+    {
+        qCritical() << "Unable to start single instance server";
+        return EXIT_FAILURE;
+    }
 
     application.setQuitOnLastWindowClosed(false);
 
