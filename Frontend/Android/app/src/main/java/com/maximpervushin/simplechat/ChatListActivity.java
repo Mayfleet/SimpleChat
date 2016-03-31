a58abb050b1fdeff06dfdb1c1361a494bc9a5b1c
Maxim Pervushin <maxim.pervushin@gmail.com>
2016-03-31 16:12:45 +0300

    Added Log In and Sign Up UI

--- Frontend/Android/app/src/main/java/com/maximpervushin/simplechat/ChatListActivity.java
+++ Frontend/Android/app/src/main/java/com/maximpervushin/simplechat/ChatListActivity.java
@@ -3,6 +3,8 @@ package com.maximpervushin.simplechat;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class ChatListActivity extends AppCompatActivity {
 
@@ -14,7 +16,7 @@ public class ChatListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chat_list_activity);
-        chatFragment = (ChatFragment)(getFragmentManager().findFragmentById(R.id.chat));
+        chatFragment = (ChatFragment) (getFragmentManager().findFragmentById(R.id.chat));
     }
 
     public void showChat(Chat chat) {
@@ -27,5 +29,39 @@ public class ChatListActivity extends AppCompatActivity {
         } else {
             chatFragment.setChat(chat);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.debug_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.show_log_in:
+                showLogIn();
+                return true;
+
+            case R.id.show_sign_up:
+                showSignUp();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void showLogIn() {
+        Intent intent = new Intent();
+        intent.setClass(this, LogInActivity.class);
+        startActivity(intent);
+    }
+
+    private void showSignUp() {
+        Intent intent = new Intent();
+        intent.setClass(this, SignUpActivity.class);
+        startActivity(intent);
     }
 }
---
+++ Frontend/Android/app/src/main/java/com/maximpervushin/simplechat/SignUpFragment.java
@@ -0,0 +1,21 @@
+package com.maximpervushin.simplechat;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by maximpervushin on 31/03/16.
+ */
+public class SignUpFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.sign_up_fragment, container, false);
+        return view;
+    }
+}
